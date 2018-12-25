7058e4fd0c125d6aaeb4e85eb4fe7cdaf6e807ad
Diego Ferreira <diegoferreiradev@gmail.com>
2018-12-25 15:55:43 -0300

    Desenvolvimento do Model e da Camada de Dados

---
+++ Core_Crud_Blazor.Server/DataAcess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Core_Crud_Blazor.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Crud_Blazor.Server.DataAcess
+{
+    public class AppDbContext : DbContext
+    {
+        public virtual DbSet<Cliente> Clientes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ Core_Crud_Blazor.Shared/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace Core_Crud_Blazor.Shared.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Pais { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
